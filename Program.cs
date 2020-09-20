12331cbe97bd74bc0af845663b2ee8ed383c1306
isaac pugliese <Isaacpugliese2010@gmail.com>
2020-09-20 14:53:26 -0300

    NmCompleto terminado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace nomecompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu Nome: ");
+            string NmUsuario = Console.ReadLine();
+            Console.Write("Digite seu Sobrenome: ");
+            string NmSobrenome = Console.ReadLine();
+            Console.Write("Nome Completo: ");
+            Console.WriteLine(NmUsuario +" "+ NmSobrenome);
+            Console.Write("Nome Catálogo: ");
+            Console.WriteLine(NmSobrenome +", "+ NmUsuario);
+        }
+    }
+}
